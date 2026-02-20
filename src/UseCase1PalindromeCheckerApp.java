b24ce1b901520e7ffb378d3b628a434207e3f8de
PragalbhRai <pragalbhrai@gmail.com>
2026-02-20 10:07:05 +0530

    UC1: Application entry and welcome message

---
+++ src/UseCase1PalindromeCheckerApp.java
@@ -0,0 +1,7 @@
+public class UseCase1PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+        System.out.println("Palindrome Checker App");
+        System.out.println("Version: 1.0");
+    }
+}
