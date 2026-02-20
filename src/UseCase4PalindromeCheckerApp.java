db9956dff3a8a1d44e8a601f2ef3aed87aeadb1f
PragalbhRai <pragalbhrai@gmail.com>
2026-02-20 10:22:33 +0530

    UC4: Character array based palindrome check using two pointers

---
+++ src/UseCase4PalindromeCheckerApp.java
@@ -0,0 +1,27 @@
+public class UseCase4PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "radar";
+        char[] chars = word.toCharArray();
+
+        int start = 0;
+        int end = chars.length - 1;
+        boolean isPalindrome = true;
+
+        while (start < end) {
+            if (chars[start] != chars[end]) {
+                isPalindrome = false;
+                break;
+            }
+            start++;
+            end--;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
