216d8689064073313b835883ffa0a2b3bb2d9b36
PragalbhRai <pragalbhrai@gmail.com>
2026-02-20 10:28:51 +0530

    UC6: Palindrome check using queue and stack (FIFO vs LIFO)

---
+++ src/UseCase6PalindromeCheckerApp.java
@@ -0,0 +1,37 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Stack;
+
+public class UseCase6PalindromeCheckerApp {
+
+    public static void main(String[] args) {
+
+        String word = "level";
+
+        Queue<Character> queue = new LinkedList<>();
+        Stack<Character> stack = new Stack<>();
+
+        // Enqueue and push characters
+        for (int i = 0; i < word.length(); i++) {
+            char ch = word.charAt(i);
+            queue.add(ch);   // FIFO
+            stack.push(ch);  // LIFO
+        }
+
+        boolean isPalindrome = true;
+
+        // Compare dequeue vs pop
+        while (!queue.isEmpty()) {
+            if (queue.remove() != stack.pop()) {
+                isPalindrome = false;
+                break;
+            }
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a Palindrome");
+        } else {
+            System.out.println(word + " is NOT a Palindrome");
+        }
+    }
+}
