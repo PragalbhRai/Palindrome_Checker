883ce86eb6919b0b27c5cb78f4a1a416b02e688f
PragalbhRai <pragalbhrai@gmail.com>
2026-02-27 10:37:41 +0530

    UC12: Strategy pattern for palindrome algorithms

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,64 @@
+import java.util.Deque;
+import java.util.ArrayDeque;
+import java.util.Stack;
+
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Palindrome Checker App");
-        System.out.println("Version: 1.0");
+        String word = "madam";
+
+        PalindromeStrategy strategy;
+
+        // Choose strategy at runtime
+        strategy = new StackStrategy();
+        System.out.println("Using Stack Strategy: " + strategy.isPalindrome(word));
+
+        strategy = new DequeStrategy();
+        System.out.println("Using Deque Strategy: " + strategy.isPalindrome(word));
+    }
+}
+
+/* Strategy Interface */
+interface PalindromeStrategy {
+    boolean isPalindrome(String word);
+}
+
+/* Stack-based Strategy */
+class StackStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean isPalindrome(String word) {
+        Stack<Character> stack = new Stack<>();
+
+        for (char c : word.toCharArray()) {
+            stack.push(c);
+        }
+
+        for (char c : word.toCharArray()) {
+            if (c != stack.pop()) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
+
+/* Deque-based Strategy */
+class DequeStrategy implements PalindromeStrategy {
+
+    @Override
+    public boolean isPalindrome(String word) {
+        Deque<Character> deque = new ArrayDeque<>();
+
+        for (char c : word.toCharArray()) {
+            deque.addLast(c);
+        }
+
+        while (deque.size() > 1) {
+            if (deque.removeFirst() != deque.removeLast()) {
+                return false;
+            }
+        }
+        return true;
     }
 }
