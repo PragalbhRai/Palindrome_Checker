d4bde74efeb170e463447a48e439f2790c85ccd6
PragalbhRai <pragalbhrai@gmail.com>
2026-02-27 10:34:49 +0530

    UC11: Object-oriented palindrome checker service

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,31 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-        System.out.println("Palindrome Checker App");
-        System.out.println("Version: 1.0");
+        PalindromeChecker checker = new PalindromeChecker();
+        String word = "madam";
+
+        if (checker.checkPalindrome(word)) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
+    }
+}
+
+// Service class that encapsulates palindrome logic
+class PalindromeChecker {
+
+    public boolean checkPalindrome(String word) {
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
     }
 }
