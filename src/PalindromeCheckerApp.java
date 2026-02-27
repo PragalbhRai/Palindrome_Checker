a1930496b2125c4d215568ff858df5c99087e928
PragalbhRai <pragalbhrai@gmail.com>
2026-02-27 10:28:20 +0530

    UC8: Palindrome check using singly linked list

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,7 +1,69 @@
 public class PalindromeCheckerApp {
 
+    // Node definition for singly linked list
+    static class Node {
+        char data;
+        Node next;
+
+        Node(char data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    // Reverse a linked list
+    static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+
+        while (current != null) {
+            Node nextNode = current.next;
+            current.next = prev;
+            prev = current;
+            current = nextNode;
+        }
+        return prev;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Palindrome Checker App");
-        System.out.println("Version: 1.0");
+        String word = "madam";
+
+        // Convert string to linked list
+        Node head = new Node(word.charAt(0));
+        Node temp = head;
+        for (int i = 1; i < word.length(); i++) {
+            temp.next = new Node(word.charAt(i));
+            temp = temp.next;
+        }
+
+        // Find middle using slow & fast pointers
+        Node slow = head;
+        Node fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        // Reverse second half
+        Node secondHalf = reverse(slow);
+
+        // Compare first and second half
+        Node firstHalf = head;
+        boolean isPalindrome = true;
+
+        while (secondHalf != null) {
+            if (firstHalf.data != secondHalf.data) {
+                isPalindrome = false;
+                break;
+            }
+            firstHalf = firstHalf.next;
+            secondHalf = secondHalf.next;
+        }
+
+        if (isPalindrome) {
+            System.out.println(word + " is a palindrome");
+        } else {
+            System.out.println(word + " is not a palindrome");
+        }
     }
 }
