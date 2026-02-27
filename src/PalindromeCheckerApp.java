b0801853fcd356b3d27c6a9144c42ced3daa98eb
kamyasinghal <kamyasinghal02@gmail.com>
2026-02-27 10:01:41 +0530

    UC1: Display Welcome message

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,32 +1,7 @@
 public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
-
-        // UC1: Welcome Message
         System.out.println("Palindrome Checker App");
         System.out.println("Version 1.0");
-        System.out.println("----------------------");
-
-        // UC2: Hardcoded Palindrome Check
-        String word = "madam";
-        boolean isPalindrome = true;
-
-        int left = 0;
-        int right = word.length() - 1;
-
-        while (left < right) {
-            if (word.charAt(left) != word.charAt(right)) {
-                isPalindrome = false;
-                break;
-            }
-            left++;
-            right--;
-        }
-
-        if (isPalindrome) {
-            System.out.println(word + " is a palindrome");
-        } else {
-            System.out.println(word + " is not a palindrome");
-        }
     }
 }
